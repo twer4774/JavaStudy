a8e6e28fae326a5f01d0733b4903a3bada80acad
yoontaehoonDev <siryou10@gmail.com>
2021-04-03 23:23:52 +0900

    0403

---
+++ Taehoon/app/src/main/java/com/taehoon/project/C0403/App.java
@@ -0,0 +1,11 @@
+package com.taehoon.project.C0403;
+
+public class App {
+	public static void main(String[] args) {
+
+		Integer a = new Integer(10);
+		Integer b = new Integer(10);
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+	}
+}
